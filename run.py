171ddd247c95c8f8d8d166f0e9ba91db9d7884a2
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2019-05-27 13:06:31 +0530

    process image cv2 -- only 5% improvement

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from prokedekt.img2str import img2string
 from prokedekt.imgEnhance import setContrast
 
 if __name__ == "__main__":
-    img1 = "/Users/satvikgolechha/prokedekt/images/lacto.jpg"
-    setContrast(2,img1,"temp.jpg")
+    img1 = "/Users/satvikgolechha/prokedekt/images/nutella-back.jpeg"
+    setContrast(1.5,img1,"temp.jpg")
     accuracy(img2string("temp.jpg"))
